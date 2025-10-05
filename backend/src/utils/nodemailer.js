25de1ae65eb2e429319242143d72db9c1f84dcca
Parvej Khan <iamparvejkhan03@gmail.com>
2025-10-05 20:17:09 +0530

    tried fixing the emails

--- backend/src/utils/nodemailer.js
+++ backend/src/utils/nodemailer.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,

871a6347509a4c67c321ec69950361640f2deb01
Parvej Khan <iamparvejkhan03@gmail.com>
2025-10-07 00:21:00 +0530

    fixed footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -132,7 +132,7 @@ function Footer() {
                     <p>
                         <span>© {new Date().getFullYear()} &nbsp;</span>
                         <Link to="/" className="underline">
-                        JD Mentors.
+                        PlaneVault.
                         </Link> 
                         &nbsp; All rights reserved. &nbsp;
                     </p>
